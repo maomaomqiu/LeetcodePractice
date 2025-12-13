7591f6c55b5995157a06e35dc2311da73c233b8c
shengyu <xiongshengyufluffy@gmail.com>
2025-12-13 05:32:25 -0800

    add leetcode 44

---
+++ DP/SequenceDP/RegxDP/lc44/Solution.java
@@ -0,0 +1,35 @@
+package DP.SequenceDP.RegxDP.lc44;
+
+class Solution {
+     public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        if (m == 0 && n == 0) return true;
+        if (n == 0) return false;
+        
+        // m >=0, n >0
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        
+        for (int i = 1; i <= n; i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+
+        if (m == 0) return dp[0][n];
+
+        // m > 0, n > 0
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                else {
+                    dp[i][j] = dp[i-1][j-1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?');
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}

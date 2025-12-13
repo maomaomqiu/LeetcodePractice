4de5b2c727ff3be285c91b6814ba088184694046
shengyu <xiongshengyufluffy@gmail.com>
2025-12-13 04:01:34 -0800

    modify

--- DP/SequenceDP/RegxDP/lc10/Solution.java
+++ DP/SequenceDP/RegxDP/lc10/Solution.java
@@ -16,20 +16,17 @@ class Solution {
 
         for (int i = 1; i <= m ; i++) {
             for (int j = 1; j <= n ; j++) {
+                // p[j] is not '*'
                 if (p.charAt(j - 1) != '*') {
                     if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.'){
                         f[i][j] = f[i-1][j-1];
                     }
                 }
                 else {
-                    if (j - 2 >= 0 && p.charAt(j - 2) == '*') {
-                        f[i][j] = f[i][j - 1];
-                    }
-                    else {
-                        boolean right = f[i - 1][j] && ((p.charAt(j - 2) == s.charAt(i - 1)) || (p.charAt(j - 2) == '.'));
-                        f[i][j] = f[i][j - 2] || right;
-                    }
-
+                    // p[j] is '*' P[j] map to j-1
+                    // p[j-1] won't be '*'
+                    boolean right = f[i - 1][j] && ((p.charAt(j - 2) == s.charAt(i - 1)) || (p.charAt(j - 2) == '.'));
+                    f[i][j] = f[i][j - 2] || right;
                 }
             }
         }
